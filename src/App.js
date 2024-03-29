253e1ad2555dcfccdad8364575f4ba412129731a
kiranbabup <kiran12146@gmail.com>
2024-03-29 18:59:53 +0530

    live test2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
 import LoginScreen from './pages/loginScreen';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/home'
 
 function App() {
   return (
-    <Router>
+    // <Router>
       <Routes>
         <Route path='/' element={<LoginScreen />} />
         <Route path='/home' element={<Home />} />
       </Routes>
-    </Router>
+    // </Router>
   );
 }
 
